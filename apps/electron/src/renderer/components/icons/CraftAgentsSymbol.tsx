6b871afbcb415a357446ebd3a671ef169f9f0798
zengkai <zengkai@bytedance.com>
2026-01-28 20:28:37 +0800

    feat: replace E symbol with bunny pixel art icon

--- apps/electron/src/renderer/components/icons/CraftAgentsSymbol.tsx
+++ apps/electron/src/renderer/components/icons/CraftAgentsSymbol.tsx
@@ -3,22 +3,35 @@ interface CraftAgentsSymbolProps {
 }
 
 /**
- * Craft Agents "E" symbol - the small pixel art icon
+ * Bunny pixel art symbol - the small pixel art bunny icon
  * Uses accent color from theme (currentColor from className)
  */
 export function CraftAgentsSymbol({ className }: CraftAgentsSymbolProps) {
   return (
     <svg
-      viewBox="452 368 115 129"
+      viewBox="0 0 16 16"
       className={className}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M474.78218,393.8 L474.78218,368 L566.666667,368 L566.666667,393.8 L474.78218,393.8 Z M521.101,419.6 L521.102306,445.4 L452,445.4 L452,393.8 L566.666667,393.8 L566.666667,419.6 L521.101,419.6 Z M474.78218,497 L474.775667,471.2 L452,471.2 L452,445.4 L566.666667,445.4 L566.666667,497 L474.78218,497 Z"
-        fill="currentColor"
-        fillRule="nonzero"
-      />
+      <defs>
+        <mask id="bunny-mask">
+          <rect x="0" y="0" width="16" height="16" fill="white" />
+          {/* Left eye cutout */}
+          <rect x="5" y="6" width="2" height="2" fill="black" />
+          {/* Right eye cutout */}
+          <rect x="9" y="6" width="2" height="2" fill="black" />
+        </mask>
+      </defs>
+      {/* Left ear */}
+      <rect x="3" y="0" width="2" height="5" fill="currentColor" />
+      {/* Right ear */}
+      <rect x="11" y="0" width="2" height="5" fill="currentColor" />
+      {/* Head with eye cutouts */}
+      <rect x="2" y="4" width="12" height="7" fill="currentColor" mask="url(#bunny-mask)" />
+      {/* Bow tie */}
+      <rect x="4" y="12" width="8" height="2" fill="currentColor" />
     </svg>
   )
 }
+
